fdf1bd6e7425ec7d7e8ae1aaf2ab13670a427288
JRyGithub <jryland@protonmail.com>
2021-01-20 16:50:09 +1300

    Need to fix footer still

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -2,27 +2,38 @@ import * as React from "react";
 import Tilt from "react-tilt";
 import "./Card.scss";
 
-const Card = ({project}) => {
-    const color = project.color;
-    const image = project.image;
-    const desc = project.description;
-    // var classes = cx({
-    //     'Tilt': true,
-    //     'br2': true,
-    //     'w-20': true,
-    //     'mr2': true,
-    //     'shadow-5': true,
-    //     'pointer': true,
-    //     'bg-color': 'bg-color-'+color
-    // });
-    console.log(project.color);
+const Card = ({ project }) => {
+  const color = project.color;
+  const image = project.image;
+  const desc = project.description;
+  // var classes = cx({
+  //     'Tilt': true,
+  //     'br2': true,
+  //     'w-20': true,
+  //     'mr2': true,
+  //     'shadow-5': true,
+  //     'pointer': true,
+  //     'bg-color': 'bg-color-'+color
+  // });
+  console.log(project.color);
   return (
-    <Tilt className={`Tilt br2 w-20 mr2 bg-base-${color} shadow-5 pointer bg-base h-50`} options={{ max: 5, perspective:500}}>
+    <Tilt
+      className={`Tilt br2 w-20 mr2 bg-base-${color} shadow-5 pointer bg-base h-50`}
+      options={{ max: 5, perspective: 500 }}
+    >
       <div className="Tilt-inner">
-        {image === null ? <div className=""></div> :
-        <img className="pa0 ma0 projectImage shadow-5" src={`${image}`} alt={`${image}`}></img>
-        }
-        
+        {image === null ? (
+          <div className=""></div>
+        ) : (
+          <>
+            <img
+              className="pa0 ma0 projectImage shadow-5"
+              src={`${image}`}
+              alt={`${image}`}
+            ></img>
+            <div className="ImageDescription"><p>{`${desc}`}</p></div>
+          </>
+        )}
       </div>
     </Tilt>
   );
--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,9 +1,9 @@
 export const ProjectData = [
-    {color: 'offWhite' , image:'https://images.unsplash.com/photo-1610894377328-d7df3b9de50d?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', desciption:'Face-Recognition (BackEnd Node JS)'},
-    {color: 'offWhite', image:'https://images.unsplash.com/photo-1610996962199-694e53bfd837?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', desciption:'Azure Function App (C#)'},
-    {color: 'offWhite', image:'https://images.unsplash.com/photo-1611001395231-bcedbab312a1?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', desciption:'Twitter Bot for FPL (C#)'},
-    {color: 'offWhite',image:'https://images.unsplash.com/photo-1611036549601-d9c142c1f602?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', desciption:'Face-Recogntion (FrontEnd React)'},
-    {color: 'offWhite',image:'https://images.unsplash.com/photo-1611067616221-213ed86ea8e6?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', desciption:'Filler Desc'},
+    {color: 'offWhite' , image:'https://images.unsplash.com/photo-1610894377328-d7df3b9de50d?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Face-Recognition (BackEnd Node)'},
+    {color: 'offWhite', image:'https://images.unsplash.com/photo-1610996962199-694e53bfd837?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Azure Function App (C#)'},
+    {color: 'offWhite', image:'https://images.unsplash.com/photo-1611001395231-bcedbab312a1?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Twitter Bot for FPL (C#)'},
+    {color: 'offWhite',image:'https://images.unsplash.com/photo-1611036549601-d9c142c1f602?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1NXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Face-Recogntion (FrontEnd React)'},
+    {color: 'offWhite',image:'https://images.unsplash.com/photo-1611067616221-213ed86ea8e6?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Filler Desc'},
 ];
 
 export default {
