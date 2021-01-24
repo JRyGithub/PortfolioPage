3dc53f8c0e3c576f46dab1b130ba73858574975e
JRyGithub <jryland@protonmail.com>
2021-01-24 16:59:40 +1300

    Links added to carsoul

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -6,16 +6,8 @@ const Card = ({ project }) => {
   const color = project.color;
   const image = project.image;
   const desc = project.description;
-  // var classes = cx({
-  //     'Tilt': true,
-  //     'br2': true,
-  //     'w-20': true,
-  //     'mr2': true,
-  //     'shadow-5': true,
-  //     'pointer': true,
-  //     'bg-color': 'bg-color-'+color
-  // });
-  console.log(project.color);
+  const urlLink = project.url;
+
   return (
     <Tilt
       className={`Tilt br2 w-20 mr2 bg-base-${color} shadow-5 pointer bg-base h-50`}
@@ -26,12 +18,14 @@ const Card = ({ project }) => {
           <div className=""></div>
         ) : (
           <>
+            <a href={`${urlLink}`}>
             <img
               className="pa0 ma0 projectImage shadow-5"
               src={`${image}`}
               alt={`${image}`}
             ></img>
             <div className="ImageDescription"><p className="ImageFont">{`${desc}`}</p></div>
+            </a>
           </>
         )}
       </div>
--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,17 +1,12 @@
 export const ProjectData = [
-    {color: 'offWhite',image:'https://p.kindpng.com/picc/s/694-6943896_github-logo-png-github-icon-png-transparent-png.png', description:'Github'},
-    {color: 'offWhite', image:'https://images.unsplash.com/photo-1600783245998-945baf9626bc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8dHdpdHRlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Twitter Bot for FPL (C#)'},
-    {color: 'offWhite',image:'https://media.istockphoto.com/photos/facial-recognition-technology-picture-id1139858627?k=6&m=1139858627&s=612x612&w=0&h=HYoOR9eZQ4fmJGuD-1Qvl93KV_PokyjspeBMHZL5zsc=', description:'Face-Recogntion (FrontEnd React)'},
-    {color: 'offWhite' , image:'https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODN8fHRlY2hub2xvZ3l8ZW58MHx8MHw%3D&auto=format&fit=crop&w=900&q=60', description:'Face-Recognition (BackEnd Node)'},
-    {color: 'primary', image:'https://www.datastax.com/sites/default/files/2020-10/azure%402x.png', description:'Azure Function App (C#)'},
-    {color: 'primary', image:'https://cdn.shopify.com/s/files/1/0510/4612/1646/files/123207074_147525810414777_6358212353268658094_o_480x480@2x.jpg?v=1605316773', description:'Skyborne Candles (Html/Css/Shopify)'}
+    {color: 'offWhite',image:'https://p.kindpng.com/picc/s/694-6943896_github-logo-png-github-icon-png-transparent-png.png', description:'Github', url:'https://github.com/JRyGithub'},
+    {color: 'offWhite', image:'https://images.unsplash.com/photo-1600783245998-945baf9626bc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8dHdpdHRlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Twitter Bot for FPL (C#)', url:'https://github.com/JRyGithub/LeedsUnitedTwitterBot'},
+    {color: 'offWhite',image:'https://raw.githubusercontent.com/JRyGithub/face-recognition/main/public/Screenshot_2021-01-24%20React%20App.jpg', description:'Face-Recogntion (FrontEnd React)', url:'https://face-recognition-new-zealand.herokuapp.com/'},
+    {color: 'offWhite' , image:'https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODN8fHRlY2hub2xvZ3l8ZW58MHx8MHw%3D&auto=format&fit=crop&w=900&q=60', description:'Face-Recognition (BackEnd Node)', url:'https://github.com/JRyGithub/face-recognition-backend'},
+    {color: 'primary', image:'https://azure.github.io/azure-sdk/images/azure-icon.png', description:'Azure Function App (C#)', url:'https://github.com/JRyGithub/AzureFunctionAppExample'},
+    {color: 'primary', image:'https://raw.githubusercontent.com/JRyGithub/face-recognition/main/public/Screenshot_2021-01-24%20Skyborne%20Candles%2C%20Wax%20and%20melts(1).jpg', description:'Skyborne Candles (Html/Css/Shopify)', url:'https://skybornecreative.com/'}
 ];
 
 export default {
     ProjectData,
 }
-
-// image:'https://images2.imgbox.com/dc/6b/zFD3W3lU_o.jpg', desciption:'Face-Recognition (BackEnd Node JS)'},
-//     {color: 'offWhite', image:'https://images2.imgbox.com/dc/e4/Ngl71fbl_o.png', desciption:'Azure Function App (C#)'},
-//     {color: 'offWhite', image:'https://images2.imgbox.com/04/50/7uZ2T9nw_o.png', desciption:'Twitter Bot for FPL (C#)'},
-//     {color: 'offWhite',image:'https://images2.imgbox.com/64/92/nZX5c6fl_o.png', d
