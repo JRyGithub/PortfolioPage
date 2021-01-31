4d0d207c63faf9d95360ea47e5835dfcc007971c
Josh Ryland <jryland@protonmail.com>
2021-01-31 13:30:41 +1300

    Added a little more project data

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,10 +1,11 @@
 export const ProjectData = [
     {color: 'offWhite',image:'https://p.kindpng.com/picc/s/694-6943896_github-logo-png-github-icon-png-transparent-png.png', description:'Github', url:'https://github.com/JRyGithub'},
-    {color: 'offWhite', image:'https://images.unsplash.com/photo-1600783245998-945baf9626bc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8dHdpdHRlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Twitter Bot for FPL (C#)', url:'https://github.com/JRyGithub/LeedsUnitedTwitterBot'},
+    {color: 'primary', image:'https://raw.githubusercontent.com/JRyGithub/face-recognition/main/public/Screenshot_2021-01-24%20Skyborne%20Candles%2C%20Wax%20and%20melts(1).jpg', description:'Skyborne Candles (Html/Css/Shopify)', url:'https://skybornecreative.com/'},
     {color: 'offWhite',image:'https://raw.githubusercontent.com/JRyGithub/face-recognition/main/public/Screenshot_2021-01-24%20React%20App.jpg', description:'Face-Recogntion (FrontEnd React)', url:'https://face-recognition-new-zealand.herokuapp.com/'},
     {color: 'offWhite' , image:'https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODN8fHRlY2hub2xvZ3l8ZW58MHx8MHw%3D&auto=format&fit=crop&w=900&q=60', description:'Face-Recognition (BackEnd Node)', url:'https://github.com/JRyGithub/face-recognition-backend'},
     {color: 'primary', image:'https://azure.github.io/azure-sdk/images/azure-icon.png', description:'Azure Function App (C#)', url:'https://github.com/JRyGithub/AzureFunctionAppExample'},
-    {color: 'primary', image:'https://raw.githubusercontent.com/JRyGithub/face-recognition/main/public/Screenshot_2021-01-24%20Skyborne%20Candles%2C%20Wax%20and%20melts(1).jpg', description:'Skyborne Candles (Html/Css/Shopify)', url:'https://skybornecreative.com/'}
+    {color: 'offWhite', image:'https://images.unsplash.com/photo-1600783245998-945baf9626bc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8dHdpdHRlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60', description:'Twitter Bot for FPL (C#)', url:'https://github.com/JRyGithub/LeedsUnitedTwitterBot'},
+    {color: 'offWhite', image:'https://raw.githubusercontent.com/JRyGithub/josh-ryland-dot-com/master/images/boat1.jpg', description:'Simple HTML/JS template', url:'https://jrygithub.github.io/josh-ryland-dot-com/#home'}
 ];
 
 export default {
